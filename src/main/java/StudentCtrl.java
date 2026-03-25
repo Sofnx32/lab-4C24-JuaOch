ff0c029807d468960d056994c9ec75ecaf57c779
Juan Carlos <juan.ochoa@tecsup.edu.pe>
2026-03-25 16:12:30 -0500

    Crear proyecto Lab02 y agregar StudentCtrl.java

---
+++ src/main/java/StudentCtrl.java
@@ -0,0 +1,21 @@
+package com.miempresa.student;
+
+public class StudentCtrl {
+
+    // Constructor
+    public StudentCtrl() {
+    }
+
+    // Métodos que controlarán la lógica del estudiante
+    public void agregarEstudiante() {
+        // TODO: implementar
+    }
+
+    public void eliminarEstudiante() {
+        // TODO: implementar
+    }
+
+    public void listarEstudiantes() {
+        // TODO: implementar
+    }
+}
